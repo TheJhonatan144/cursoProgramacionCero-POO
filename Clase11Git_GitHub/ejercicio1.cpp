c8995b2f31133e1198531e7dd9d2a9cc08e18ce0
Jhonatan Torres <jhonatan_pts144@hotmail.com>
2026-03-20 21:23:19 -0500

    clase 11 - mejorar presentacion del programa y documentar el codigo

--- Clase11Git_GitHub/ejercicio1.cpp
+++ Clase11Git_GitHub/ejercicio1.cpp
@@ -14,11 +14,14 @@ using namespace std;
 
 int main()
 {
-
+    // Variables para almacenar las 3 notas y el promedio
     double n1, n2, n3, promedio;
 
-    cout << "=== SISTEMA SIMPLE DE PROMEDIO ===" << endl;
+    cout << "==========================" << endl;
+    cout << "SISTEMA SIMPLE DE PROMEDIO" << endl;
+    cout << "==========================" << endl;
 
+    // Ingreso y validacion de la primera nota
     cout << "Ingrese la nota 1: ";
     cin >> n1;
     while (n1 < 0 || n1 > 10)
@@ -27,6 +30,7 @@ int main()
         cin >> n1;
     }
 
+    // Ingreso y validacion de la segunda nota
     cout << "Ingrese la nota 2: ";
     cin >> n2;
     while (n2 < 0 || n2 > 10)
@@ -35,6 +39,7 @@ int main()
         cin >> n2;
     }
 
+    // Ingreso y validacion de la tercera nota
     cout << "Ingrese la nota 3: ";
     cin >> n3;
     while (n3 < 0 || n3 > 10)
@@ -43,10 +48,12 @@ int main()
         cin >> n3;
     }
 
+    // calcular el promedio
     promedio = (n1 + n2 + n3) / 3;
 
     cout << "Promedio final: " << promedio << endl;
 
+    // Evaluacion del estado final
     if (promedio >= 7)
     {
         cout << "Estado: APROBADO" << endl;
